8e2735d39e65e9ea638e0ff3ce09883ac2ca48d5
Carlos Ferreira <carloscjr98@gmail.com>
2021-01-06 01:53:13 -0300

    Updating geolocation on seasons app

--- 02-seasons/src/index.js
+++ 02-seasons/src/index.js
@@ -1,9 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>oi</div>;
-};
+/* 
+    Func Based
+*/
+
+// const App = () => {
+//
+//   return (
+//     <div>
+//       <div>Latitude: </div>
+//     </div>
+//   );
+// };
+
+/*
+    Class Based
+*/
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null };
+    window.navigator.geolocation.getCurrentPosition(
+      (pos) => {
+        this.setState({ lat: pos.coords.latitude });
+      },
+      (err) => console.log(err)
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <div>Latitude: {this.state.lat}</div>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(
   <React.StrictMode>

18029977af4b9522b049b92c667ad5840ed3cb24
Carlos Alberto Ferreira Junior <carlos.ferreira@insightlab.ufc.br>
2021-01-06 12:20:13 -0300

    Conditional rendering

--- 02-seasons/src/index.js
+++ 02-seasons/src/index.js
@@ -20,21 +20,32 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { lat: null };
+    this.state = { lat: null, errMsg: '' };
     window.navigator.geolocation.getCurrentPosition(
       (pos) => {
         this.setState({ lat: pos.coords.latitude });
       },
-      (err) => console.log(err)
+      (err) => {
+        this.setState({ errMsg: err.message });
+      }
     );
   }
 
   render() {
-    return (
-      <div>
-        <div>Latitude: {this.state.lat}</div>
-      </div>
-    );
+    // return (
+    //   <div>
+    //     <div>Latitude: {this.state.lat}</div>
+    //     <br></br>
+    //     <div>Error: {this.state.errMsg}</div>
+    //   </div>
+    // );
+    if (!this.state.lat && this.state.errMsg) {
+      return <div>Error: {this.state.errMsg}</div>;
+    }
+    if (this.state.lat && !this.state.errMsg) {
+      return <div>Latitude: {this.state.lat}</div>;
+    }
+    return <div>loading...</div>;
   }
 }
 

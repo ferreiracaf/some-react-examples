0edec669a0e86ab95cbcacd3b082026b986ef8c2
ferreiracaf <carloscjr98@gmail.com>
2021-03-10 15:34:12 -0300

    Updating Seasons App

--- 02-seasons/src/index.js
+++ 02-seasons/src/index.js
@@ -1,51 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
+import LoaderSpinning from './LoaderSpinning';
 
-/* 
-    Func Based
-*/
-
-// const App = () => {
-//
-//   return (
-//     <div>
-//       <div>Latitude: </div>
-//     </div>
-//   );
-// };
-
-/*
-    Class Based
-*/
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null, errMsg: '' };
+  state = { lat: null, errMsg: '' };
+
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        this.setState({ lat: pos.coords.latitude });
-      },
-      (err) => {
-        this.setState({ errMsg: err.message });
-      }
+      (pos) => this.setState({ lat: pos.coords.latitude }),
+      (err) => this.setState({ errMsg: err.message })
     );
   }
 
-  render() {
-    // return (
-    //   <div>
-    //     <div>Latitude: {this.state.lat}</div>
-    //     <br></br>
-    //     <div>Error: {this.state.errMsg}</div>
-    //   </div>
-    // );
+  componentDidUpdate() {
+    // console.log('Updated Component');
+  }
+
+  renderContent() {
     if (!this.state.lat && this.state.errMsg) {
       return <div>Error: {this.state.errMsg}</div>;
     }
     if (this.state.lat && !this.state.errMsg) {
-      return <div>Latitude: {this.state.lat}</div>;
+      return <SeasonDisplay lat={this.state.lat} />;
     }
-    return <div>loading...</div>;
+    return <LoaderSpinning message='Please, accept location request' />;
+  }
+
+  render() {
+    return <div className='border red'>{this.renderContent()}</div>;
   }
 }
 
